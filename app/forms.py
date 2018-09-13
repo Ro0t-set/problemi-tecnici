ab4d10f35fd7438403e42df3bec6ee58ed35a511
arcobiSwifty <33202233+arcobiSwifty@users.noreply.github.com>
2018-09-13 21:09:18 +0200

    ui changes, bootstrap and table structure for /problemi

--- app/forms.py
+++ app/forms.py
@@ -12,4 +12,7 @@ from .models import Aula,Problema
 class PubbicaProblema(forms.ModelForm):
     class Meta:
         model = Problema
+        widgets = {
+            'descrizioneProblema': forms.Textarea(attrs={'class': 'form-control', 'rows': '3', 'placeholder': 'Descrivi il problema'}),
+        }
         fields = ['classe','descrizioneProblema']
